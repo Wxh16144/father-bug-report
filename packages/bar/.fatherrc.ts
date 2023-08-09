1e170803dcba6650d574b073504c2898c89f9ae3
wuxh <wxh1220@gmail.com>
2023-08-09 11:24:57 +0800

    bug: 添加 umd 构建后会报错

--- packages/bar/.fatherrc.ts
+++ packages/bar/.fatherrc.ts
@@ -2,4 +2,13 @@ import { defineConfig } from 'father';
 
 export default defineConfig({
   extends: '../../.fatherrc.base.ts',
+  umd: {
+    entry: {
+      'src/utils/index.ts': {
+        output: 'dist',
+        name: 'barUtils',
+        platform: 'browser',
+      }
+    }
+  }
 });
